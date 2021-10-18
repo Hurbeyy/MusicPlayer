eae5a7e18b002782b7c3d3b341419cfead586ca3
Hurbeyy <92626103+Hurbeyy@users.noreply.github.com>
2021-10-18 23:47:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -61,6 +61,12 @@ class Config:
     ADMIN = os.environ.get("ADMINS", '')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     API_ID = int(os.environ.get("API_ID", ''))
+    CHAT = int(os.environ.get("CHAT", ""))
+    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    if LOG_GROUP:
+        LOG_GROUP=int(LOG_GROUP)
+    else:
+        LOG_GROUP=None
     STREAM_URL=finalurl
     CPLAY=C_PLAY
     YPLAY=Y_PLAY
